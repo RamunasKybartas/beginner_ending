330263491753cc5e061ce0c7600722f5883cfdb4
RamunasKybartas <kybartas.ramunas@gmail.com>
2022-09-29 16:56:50 +0300

    main code brain done

--- main.py
+++ main.py
@@ -15,24 +15,42 @@ print(zodis)
 def pasleptas_zodis():
     spetos_raides = []
     zodzio_raides = []
-    slaptas_zodis = []
+    slaptos_raides = []
+    gyvybes = 6
     for raide in zodis:
         zodzio_raides.append(raide)
 
-    while len(zodzio_raides) > 0:
+    for zvaigzdute in range(len(zodis)):
+            slaptos_raides.append("*")
+
+    while len(zodzio_raides) > 0 and gyvybes > 0:
         
-        print(f"spetos raides: {' '.join(spetos_raides)}")
+        print("Zodis: "," ".join(slaptos_raides))
         speta_raide = input("Įveskite raidę: ").upper()
-        if speta_raide not in spetos_raides:
-            spetos_raides.append(speta_raide)
+        print(f"spetos raides: {' '.join(spetos_raides)}")
+        
+        for i in range(len(zodis)):
+            if speta_raide == zodis[i]:
+                slaptos_raides[i] = speta_raide
+        
         if speta_raide in zodzio_raides:
             while speta_raide in zodzio_raides: zodzio_raides.remove(speta_raide)
-            # zodzio_raides.remove(speta_raide)
-        elif speta_raide in spetos_raides:
-            print("Jus jau spejote sia raide") 
-    
-        print(slaptas_zodis)
+        else:
+            gyvybes -= 1
+        
+        if speta_raide not in spetos_raides:
+            spetos_raides.append(speta_raide)
+        else:
+            print("Jus jau spejote sia raide")    
+        
+    if gyvybes == 0:
+            print("Jums pritruko gyvybiu")
+    else:
+        print("Sveikiname, atspejote zodi")
+        
     print(zodis)
 
 pasleptas_zodis()
       
+
+            
